0930f4e30b863204508eb1e8f09cbf9510d2bb40
Guy Warburg <thexblue@gmail.com>
2019-04-06 21:21:40 +0300

    add image and improve text

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,14 @@ import Layout from "../styles/Layout"
 
 import "../styles/global.css"
 
+import hammock from "../../public/Group 1.png"
+
 const StyledHomePage = styled.div`
   margin-top: 5rem;
   padding-left: 4rem;
+  display: grid;
+  grid-template-columns: 500px 1fr;
+  grid-gap: 3rem;
 `
 
 const StyledTitle = styled.h1`
@@ -19,15 +24,20 @@ const StyledBodyText = styled.p`
   font-weight: 400;
   font-size: 1rem;
   color: hsl(35, 71%, 74%);
+  line-height: 1.5rem;
 `
 
 export default () => (
   <Layout>
     <StyledHomePage>
-      <StyledTitle>Hi there!</StyledTitle>
-      <StyledBodyText>
-        My name is Guy Warburg and this is my site.
-      </StyledBodyText>
+      <div>
+        <StyledTitle>Hammock Thoughts</StyledTitle>
+        <StyledBodyText>
+          My name is Guy Warburg and this is my site. I'm still in the hammock
+          but I hope to post some stuff real soon.
+        </StyledBodyText>
+      </div>
+      <img src={hammock} />
     </StyledHomePage>
   </Layout>
 )
